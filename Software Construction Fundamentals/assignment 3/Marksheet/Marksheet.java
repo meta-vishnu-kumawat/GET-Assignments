3f1957d49401f16355ce48614147073cd65bd621
vishnu kumawat <vishnu.kumawat@metacube.com>
2025-03-10 12:17:15 +0530

    updation

--- Software Construction Fundamentals/assignment 3/Marksheet/Marksheet.java
+++ Software Construction Fundamentals/assignment 3/Marksheet/Marksheet.java
@@ -12,10 +12,11 @@ public class Marksheet {
     MarksheetCalculations m = new MarksheetCalculations(n);
     m.marksheetCalculation();
     System.out.println("===========================================");
-    System.out.println("Avarage grade of Students is : " + m.avrengeGrade());
-    System.out.println("Maximum grade of Students is : " + m.maximumGrade());
-    System.out.println("Minimum grade of Students is : " + m.manimumGrade());
-    System.out.println("Parcentage of  Students  passed is : " + m.parcentageOfStudentsPassed() + "%");
+    System.out.printf("Avarage grade of Students is :     %.2f\n ", m.avrengeGrade());
+    System.out.println("Maximum grade of Students is :    "+ m.maximumGrade());
+    System.out.println("Minimum grade of Students is :     "+ m.manimumGrade());
+    System.out.printf("Parcentage of Students passed is : %.2f",m.parcentageOfStudentsPassed());
+    System.out.print("%");
 
     sc.close();
   }
